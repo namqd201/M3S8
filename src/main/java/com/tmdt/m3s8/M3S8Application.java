cfeb8706ffdd2887f7e46724fb27de2bfdd1360f
namqd201 <namqd201@gmail.com>
2026-03-02 15:10:05 +0700

    ups8

---
+++ src/main/java/com/tmdt/m3s8/M3S8Application.java
@@ -0,0 +1,13 @@
+package com.tmdt.m3s8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3S8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M3S8Application.class, args);
+    }
+
+}
